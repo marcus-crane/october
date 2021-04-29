03f350de6137c1df17eb0cc9ba82faa172e20b48
Marcus Crane <marcus@utf9k.net>
2021-04-29 20:51:17 +1200

    Got a basic reducer test up and running

---
+++ src/reducer.jsx
@@ -0,0 +1,6 @@
+export default function reducer (store = {}, action) {
+  switch(action.type) {
+    default:
+      return store
+  }
+}
---
+++ src/reducer.test.jsx
@@ -0,0 +1,10 @@
+import reducer from './reducer'
+
+describe('reducer', () => {
+  it('should return the initial state when receiving an undefined action', () => {
+    const expected = {}
+    const store = undefined
+    const action = 'UNKNOWN'
+    expect(reducer(store, action)).toEqual(expected)
+  })
+})
