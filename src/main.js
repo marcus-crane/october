29dc41aad8b2618ea6d69085b41a1aebb6fcf019
Marcus Crane <marcus@utf9k.net>
2021-04-30 20:59:59 +1200

    Add image credits

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY) // eslint-disable-line
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
