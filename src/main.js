9f959c0239f012e59d102452a6cd4a1e5051ad24
Marcus Crane <marcus@utf9k.net>
2021-05-01 21:58:54 +1200

    Fix event not being used for linting

--- src/main.js
+++ src/main.js
@@ -48,4 +48,4 @@ app.on("activate", () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-ipcMain.handle('select-mounted-volume', event => dialog.showOpenDialogSync({ properties: ['openDirectory'] })
+ipcMain.handle("select-mounted-volume", () => dialog.showOpenDialogSync({ properties: ["openDirectory"] }))
