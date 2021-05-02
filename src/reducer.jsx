ab741c6006cff9eda46408ceaa3aa6a29c50f321
Marcus Crane <marcus@utf9k.net>
2021-05-02 18:47:54 +1200

    Add some initial reducer tests

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,6 +1,28 @@
+import { actionTypes } from "./actions.jsx"
+
 export default function reducer(store = {}, action) {
   switch (action.type) {
+    case actionTypes.GET_DB_PATH_SUCCESS:
+      return getDbPathSuccess(store, action)
+    case actionTypes.GET_DB_PATH_FAILURE:
+      return getDbPathFailure(store, action)
     default:
-      return store;
+      return store
+  }
+}
+
+const getDbPathSuccess = (store, action) => {
+  const { databasePath } = action
+  return {
+    ...store,
+    databasePath,
+  }
+}
+
+const getDbPathFailure = (store, action) => {
+  const { errorMessage } = action
+  return {
+    ...store,
+    errorMessage,
   }
 }
--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,10 +1,33 @@
-import reducer from "./reducer";
+import reducer from "./reducer"
+import { actionTypes } from "./actions.jsx"
 
 describe("reducer", () => {
   it("should return the initial state when receiving an undefined action", () => {
-    const expected = {};
-    const store = undefined;
-    const action = "UNKNOWN";
-    expect(reducer(store, action)).toEqual(expected);
-  });
-});
+    const expected = {}
+    const store = undefined
+    const action = "UNKNOWN"
+    expect(reducer(store, action)).toEqual(expected)
+  })
+
+  it("should handle GET_DB_PATH_SUCCESS", () => {
+    const databasePath = "/tmp/.kobo/KoboReader.sqlite"
+    const expected = { databasePath }
+    const store = {}
+    const action = {
+      type: actionTypes.GET_DB_PATH_SUCCESS,
+      databasePath,
+    }
+    expect(reducer(store, action)).toEqual(expected)
+  })
+
+  it("should handle GET_DB_PATH_FAILURE", () => {
+    const errorMessage = "is your hard drive running?"
+    const expected = { errorMessage }
+    const store = {}
+    const action = {
+      type: actionTypes.GET_DB_PATH_FAILURE,
+      errorMessage,
+    }
+    expect(reducer(store, action)).toEqual(expected)
+  })
+})
