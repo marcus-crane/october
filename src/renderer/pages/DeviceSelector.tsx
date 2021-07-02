8e6a46ce09c161c594972911bc5fe7c6d59d3d1d
Marcus Crane <marcus@utf9k.net>
2021-07-02 23:49:01 +1200

    Fix typo on DeviceSelector

--- src/renderer/pages/DeviceSelector.tsx
+++ src/renderer/pages/DeviceSelector.tsx
@@ -26,7 +26,7 @@ class DeviceSelector extends Component {
             type="submit"
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 dark:bg-indigo-500 hover:bg-indigo-700 dark:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Select your conncted Kobo device
+            Select your connected Kobo device
           </button>
         </div>
       </div>
