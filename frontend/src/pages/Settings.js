4f417d8d4b8c792bd4825635236b408dd8da0929
Marcus Crane <marcus@utf9k.net>
2022-01-04 10:44:23 +1300

    Convert other components to use hooks

--- frontend/src/pages/Settings.js
+++ frontend/src/pages/Settings.js
@@ -1,23 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Settings extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      settings: {}
-    }
-  }
-  render() {
-    return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800 py-12 px-24 grid grid-cols-2 gap-14">
-        <div className="space-y-2">
-          <h2 className="text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
-            Settings
-          </h2>
-        </div>
+export default function Settings() {
+  const [settings, setSettings] = useState({})
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800 py-12 px-24 grid grid-cols-2 gap-14">
+      <div className="space-y-2">
+        <h2 className="text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+          Settings
+        </h2>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Settings
