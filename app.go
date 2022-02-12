b5414127965e2234828629305b88f94259f4892c
Marcus Crane <marcus@utf9k.net>
2022-02-12 20:31:50 +1300

    Add winfile logger workaround

--- app.go
+++ app.go
@@ -22,6 +22,15 @@ type App struct {
 	KoboService *KoboService
 }
 
+// newWinFileSink creates a log sink on Windows machines as zap, by default,
+// doesn't support Windows paths. A workaround is to create a fake winfile
+// scheme and register it with zap instead. This workaround is taken from
+// the Github issue at https://github.com/uber-go/zap/issues/621
+func newWinFileSink(u *url.URL) (zap.Sink, error) {
+	// Remove leading slash left by url.Parse()
+	return os.OpenFile(u.Path[1:], os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+}
+
 // NewApp creates a new App application struct
 func NewApp() (*App, error) {
 	configPath, err := xdg.ConfigFile(configFilename)
@@ -38,6 +47,10 @@ func NewApp() (*App, error) {
 	logFile := fmt.Sprintf("october/logs/%s.log", time.Now().Format("2006-01-02"))
 	logPath, err := xdg.DataFile(logFile)
 	config := zap.NewProductionConfig()
+	if runtime.GOOS == "windows" {
+		zap.RegisterSink("winfile", newWinFileSink)
+		logPath = "winfile:///" + logPath
+	}
 	config.OutputPaths = []string{"stdout", logPath}
 	logger, err := config.Build()
 	if err != nil {
