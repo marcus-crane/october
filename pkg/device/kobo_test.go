1505251eadccd1567110310d5e17d524f5c43f6c
Marcus Crane <marcus@utf9k.net>
2022-03-13 15:04:20 +1300

    Add Kobo detection test

--- pkg/device/kobo_test.go
+++ pkg/device/kobo_test.go
@@ -8,16 +8,25 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/marcus-crane/october/pkg/logger"
 )
 
-func setupTmpKobo() string {
-	content := []byte("NXXXXXXXXXX,4.1.15,4.30.18838,4.1.15,4.1.15,00000000-0000-0000-0000-000000000388")
-	dir, err := ioutil.TempDir("", "KOBOeReader")
-	if err != nil {
-		log.Fatal(err)
-		return ""
-	}
-	defer os.RemoveAll(dir)
+var (
+	elipsaDeviceId   = "NXXX,4.9.56,4.30.18838,4.9.56,4.9.56,00000000-0000-0000-0000-000000000387"
+	libraTwoDeviceId = "NXXXXXXXXXX,4.1.15,4.30.18838,4.1.15,4.1.15,00000000-0000-0000-0000-000000000388"
+	miniDeviceId     = "NXXX,4.9.56,4.30.18838,4.9.56,4.9.56,00000000-0000-0000-0000-000000000340"
+)
+
+func TestMain(m *testing.M) {
+	logger.Init()
+	code := m.Run()
+	os.Exit(code)
+}
+
+// We receive a tempdir from the test runner itself, which will handle cleanup for us.
+func setupTmpKobo(dir string, deviceId string) string {
+	content := []byte(deviceId)
 	os.Mkdir(filepath.Join(dir, ".kobo"), 0777)
 	tmpfn := filepath.Join(dir, ".kobo", "version")
 	if err := ioutil.WriteFile(tmpfn, content, 0666); err != nil {
@@ -33,14 +42,46 @@ func TestGetKoboMetadata_HandleNoDevices(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestGetKoboMetadata_HandleConnectedDevice(t *testing.T) {
-	fakeKoboVolume := setupTmpKobo()
+// If you were to disconnect a device at the precise time between getting the connected paths
+// and checking the version at those paths, it would look the same as trying to access a file
+// that doesn't exist
+func TestGetKoboMetadata_HandleDisconnectedDevice(t *testing.T) {
+	var expected []Kobo
+	actual := GetKoboMetadata([]string{os.TempDir()})
+	assert.Equal(t, expected, actual)
+}
+
+func TestGetKoboMetadata_HandleConnectedDevices(t *testing.T) {
+	libraTempDir := t.TempDir()
+	fakeLibraVolume := setupTmpKobo(libraTempDir, libraTwoDeviceId)
+	miniTempDir := t.TempDir()
+	fakeMiniVolume := setupTmpKobo(miniTempDir, miniDeviceId)
+	elipsaTempDir := t.TempDir()
+	fakeElipsaVolume := setupTmpKobo(elipsaTempDir, elipsaDeviceId)
 	expected := []Kobo{
 		{
-			Name: "Kobo eReader",
+			Name:       "Kobo Libra 2",
+			Storage:    32,
+			DisplayPPI: 300,
+			MntPath:    libraTempDir,
+			DbPath:     filepath.Join(libraTempDir, "/.kobo/KoboReader.sqlite"),
+		},
+		{
+			Name:       "Kobo Mini",
+			Storage:    2,
+			DisplayPPI: 200,
+			MntPath:    miniTempDir,
+			DbPath:     filepath.Join(miniTempDir, "/.kobo/KoboReader.sqlite"),
+		},
+		{
+			Name:       "Kobo Elipsa",
+			Storage:    32,
+			DisplayPPI: 227,
+			MntPath:    elipsaTempDir,
+			DbPath:     filepath.Join(elipsaTempDir, "/.kobo/KoboReader.sqlite"),
 		},
 	}
-	detectedPaths := []string{fakeKoboVolume}
+	detectedPaths := []string{fakeLibraVolume, fakeMiniVolume, fakeElipsaVolume}
 	actual := GetKoboMetadata(detectedPaths)
 	assert.Equal(t, expected, actual)
 }
