58a18604f75a21275636f5945f073b3ca04bce62
Marcus Crane <marcus@utf9k.net>
2022-03-21 22:28:42 +1300

    Fix Windows redirect

--- scripts/generate-redirects.py
+++ scripts/generate-redirects.py
@@ -6,7 +6,7 @@ data = r.json()
 version = data["tag_name"]
 
 mac_redirect = f"/download/mac/latest https://github.com/marcus-crane/october/releases/download/{version}/october-darwin-universal-{version}.dmg"
-win_redirect = f"/download/win/latest https://github.com/marcus-crane/october/releases/download/{version}/october-windows-amd64-{version}.dmg"
+win_redirect = f"/download/win/latest https://github.com/marcus-crane/october/releases/download/{version}/october-windows-amd64-{version}.zip"
 
 with open("site/_redirects", "w") as file:
     file.write(mac_redirect + "\n")
