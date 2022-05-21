74e59ddab6426612f6da6607c9e9d575b50977ef
Marcus Crane <marcus@utf9k.net>
2022-05-21 16:12:05 +1200

    Use pnpm and hide device chooser button when on homepage

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -21,12 +21,14 @@ export default function Navbar() {
       </div>
       <div className="-mr-2 -my-2">
         <div className="flex items-center ml-12">
+          {location.pathname !== "/" && (
           <a
             href="#"
             className="text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Pick another device
           </a>
+          )}
           <a
             href="#"
             className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
