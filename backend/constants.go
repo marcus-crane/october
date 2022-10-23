49cb85f053bbb7c5822e46649529680bdd34bb6c
Marcus Crane <marcus@utf9k.net>
2022-10-23 17:56:46 +1300

    Add comment about true max length

--- backend/constants.go
+++ backend/constants.go
@@ -6,7 +6,7 @@ var (
 	configFilename     = "october/config.json"
 	CoverEndpoint      = "https://readwise.io/api/v2/books/%d"
 	HighlightsEndpoint = "https://readwise.io/api/v2/highlights/"
-	MaxHighlightLen    = 8096
+	MaxHighlightLen    = 8096 // It's actually 8191 but we'll go under the limit anyway
 	SourceCategory     = "books"
 	SourceType         = "OctoberForKobo"
 	UserAgent          = "october/2.0.0 <https://github.com/marcus-crane/october>"
