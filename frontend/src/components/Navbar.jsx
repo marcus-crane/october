db70e6c860f1ebb23224a6183ab0fb51d0113497
Marcus Crane <marcus@utf9k.net>
2023-05-28 16:00:59 +1200

    Upgrade hero-icons package to next major version
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom"
-import { BookmarkIcon, ChipIcon, CogIcon } from '@heroicons/react/solid'
+import { BookmarkIcon, CpuChipIcon, CogIcon } from '@heroicons/react/20/solid'
 
 export default function Navbar() {
   const location = useLocation()
   return (
     <header className="flex p-3">
       <div className="w-full text-left">
-        {location.pathname === "/overview" && <NavLink to="/"><ChipIcon className="h-5 w-5 inline-block" /> Pick a different device</NavLink>}
+        {location.pathname === "/overview" && <NavLink to="/"><CpuChipIcon className="h-5 w-5 inline-block" /> Pick a different device</NavLink>}
         {location.pathname === "/settings" && <NavLink to="/overview"><BookmarkIcon className="h-5 w-5 inline-block" /> Return to device overview</NavLink>}
       </div>
       <div className="w-full text-right">
