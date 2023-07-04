bc78bc86e55fa50141365f1be7b9ca1945a2d1c7
Marcus Crane <marcus@utf9k.net>
2023-07-04 19:11:34 +1200

    Fix up asset paths and generate Linuux redirect for docs

--- scripts/generate-redirects.py
+++ scripts/generate-redirects.py
@@ -4,10 +4,13 @@ url = "https://api.github.com/repos/marcus-crane/october/releases/latest"
 r = requests.get(url)
 data = r.json()
 version = data["tag_name"]
+normalised_version = version.replace("v", "")
 
-mac_redirect = f"/download/mac/latest https://github.com/marcus-crane/october/releases/download/{version}/october-darwin-universal-{version}.dmg"
-win_redirect = f"/download/win/latest https://github.com/marcus-crane/october/releases/download/{version}/october-windows-amd64-{version}.zip"
+mac_redirect = f"/download/mac/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_darwin_universal.dmg"
+win_redirect = f"/download/win/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_windows_amd64.zip"
+linux_redirect = f"/download/linux/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_linux_amd64.deb"
 
 with open("site/_redirects", "w") as file:
     file.write(mac_redirect + "\n")
     file.write(win_redirect + "\n")
+    file.write(linux_redirect + "\n")
