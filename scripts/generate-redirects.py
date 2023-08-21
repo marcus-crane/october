213011ec7d056d7bc5af1936b72a1a3adfe3fc79
Marcus Crane <marcus@utf9k.net>
2023-08-22 11:35:42 +1200

    Fix Windows Portable naming

--- scripts/generate-redirects.py
+++ scripts/generate-redirects.py
@@ -8,10 +8,11 @@ normalised_version = version.replace("v", "")
 
 mac_redirect = f"/download/mac/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_darwin_universal.zip"
 win_redirect = f"/download/win/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_windows_amd64.zip"
-win_redirect = f"/download/win-portable/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_windows-portable_amd64.zip"
+win_portable_redirect = f"/download/win-portable/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_windows-portable_amd64.zip"
 linux_redirect = f"/download/linux/latest https://github.com/marcus-crane/october/releases/download/{version}/october_{normalised_version}_linux_amd64.zip"
 
 with open("site/_redirects", "w") as file:
     file.write(mac_redirect + "\n")
     file.write(win_redirect + "\n")
+    file.write(win_portable_redirect + "\n")
     file.write(linux_redirect + "\n")
