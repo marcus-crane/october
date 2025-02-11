300adea85c0f9ac0aef7d947c64c05aefa813440
Marcus Crane <marcus@utf9k.net>
2025-02-11 22:59:25 +1300

    Refresh epub experiment
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- v2/pkg/epub/epub.go
+++ v2/pkg/epub/epub.go
@@ -1,36 +1,45 @@
 package epub
 
-import (
-	"fmt"
-	"os"
+import "archive/zip"
 
-	"github.com/taylorskalyo/goreader/epub"
-)
+type Epub struct {
+	zip *zip.ReadCloser
 
-// NOTE TO SELF: Cover images can be retrieved like so:
-// 1) Find meta name="cover" in OPF metadata and check the content
-// 2) Find item entry with id set to same value as metadata content
-// See https://github.com/kobolabs/epub-spec#cover-images
-// Also covers may technically be SVGs so check the types and
-// explicitly mention what is supported
+	Package  map[string]string
+	Metadata []MetadataItem
+	Manifest map[string]ManifestItem
+	Spine    []SpineItem
+	Guide    []GuideItem
+}
 
-// See https://github.com/kobolabs/epub-spec#for-epub3 for a list of nav fallbacks
-// if OPF doesn't have the desired information
-
-// LoadEpub takes a relative path to an epub file (aka VolumeID) and returns the content of the epub
-// Further parsing is required to actually read files for example but it gives a full book spine
-// among other useful metadata that may or may not be present in the database, particular with
-// epubs (not kepubs) that have been loaded directly onto the device without using a tool like Calibre
-func LoadEpub(path string) (*epub.Rootfile, error) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return nil, err
+func (e Epub) GetTitle() string {
+	for _, item := range e.Metadata {
+		if item.Name == "dc:title" {
+			return item.Value
+		}
 	}
-	rc, err := epub.OpenReader(path)
-	if err != nil {
-		return nil, err
-	}
-	if len(rc.Rootfiles) == 0 {
-		return nil, fmt.Errorf("no root files found in epub")
-	}
-	return rc.Rootfiles[0], nil
+	return e.zip.File[0].Name
+}
+
+type MetadataItem struct {
+	Name  string
+	Value string
+	Attrs map[string]string
+}
+
+type ManifestItem struct {
+	ID        string
+	Href      string
+	MediaType string
+}
+
+type SpineItem struct {
+	IDRef  string
+	Linear bool
+}
+
+type GuideItem struct {
+	Href  string
+	Title string
+	Type  string
 }
